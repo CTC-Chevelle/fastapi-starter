24cb1472ca922b2eb9e59b5568da6f0496fe1c2b
CTC-Chevelle <chevelle.code.23@gmail.com>
2026-05-06 16:37:04 -0400

    Fix static and template paths for Docker

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -4,12 +4,14 @@ from fastapi.staticfiles import StaticFiles
 from app.utilities.flash import get_flashed_messages
 from jinja2 import Environment, FileSystemLoader
 from app.config import get_settings
+import os
 
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-template_env = Environment(loader = FileSystemLoader("app/templates",), )
+template_env = Environment(loader=FileSystemLoader(os.path.join(base_dir, "templates")))
 template_env.globals['get_flashed_messages'] = get_flashed_messages
 templates = Jinja2Templates(env=template_env)
-static_files = StaticFiles(directory="app/static")
+static_files = StaticFiles(directory=os.path.join(base_dir, "static"))
 
 router = APIRouter(tags=["Jinja Based Endpoints"], include_in_schema=get_settings().env.lower() in ["dev","development"])
 api_router = APIRouter(tags=["API Endpoints"], prefix="/api")
